ea58fd2ffc19ce5b9f7d45c00ed9b334dd95cf72
HappyRoma <krivonogov.roma@mail.ru>
2024-12-24 14:48:42 +0500

    FastAPI CORS added

--- app-fast-api-ood/main.py
+++ app-fast-api-ood/main.py
@@ -3,9 +3,19 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 from io import BytesIO
 from PIL import Image
+from fastapi.middleware.cors import CORSMiddleware
+
 from model_ood import detect_objects
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
